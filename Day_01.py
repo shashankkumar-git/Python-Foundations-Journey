382bef07c8094762231551bc4cabdd132ed50643
shashankkumar-git <shashank.cm.kumar@gmail.com>
2026-05-17 00:00:57 +0530

    Update Day_01.py

--- Day_01.py
+++ Day_01.py
@@ -1,4 +1,4 @@
-#PRONLEM STATEMENT - Take the string text = " Python_Programming_is_fun ". Clean the whitespace and replace the underscores with spaces
+#PROBLEM STATEMENT - Take the string text = " Python_Programming_is_fun ". Clean the whitespace and replace the underscores with spaces
 
 text = " Python_Programming_is_fun "
 text = text.strip()
