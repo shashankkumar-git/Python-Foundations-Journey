48d298a1822afd0ccd61abc90f8f30a0df54d0ed
Shashank Kumar <shashank.cm.kumar@gmail.com>
2026-05-26 10:35:12 +0530

    New Project

---
+++ Auto-Folder Cleaner/Folder_Cleaner.py
@@ -0,0 +1,19 @@
+import os
+
+target_dir = input("Please provide the path of the file to be cleaned: ")
+
+def intialize_folder(folder_path):
+    print("*"*15 ,"AUTOMATED FOLDER CLEANER ACTIVATED", "*"*15, "\n")
+
+    if os.path.exists(folder_path):
+        
+        folder_items = os.listdir(folder_path)
+
+        print(f"Total items in the folder is: {len(folder_items)}")
+        print(f"The items in the file are as follows:\n", folder_items)
+
+    else:
+        print("The given path file doesn't exists")
+
+intialize_folder(target_dir)
+
