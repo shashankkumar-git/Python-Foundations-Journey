8d272688338b7b7376a76500dd50eb0b48a7dc5a
Shashank Kumar <shashank.cm.kumar@gmail.com>
2026-05-27 09:05:42 +0530

    Implemented tuple unpacking with splitext to segregate files and folders

--- Auto-Folder Cleaner/Folder_Cleaner.py
+++ Auto-Folder Cleaner/Folder_Cleaner.py
@@ -12,8 +12,18 @@ def intialize_folder(folder_path):
         print(f"Total items in the folder is: {len(folder_items)}")
         print(f"The items in the file are as follows:\n", folder_items)
 
+        folder = []
+        files = []
+
+        for items in folder_items:
+            name, extension = os.path.splitext(items)
+            
+            if extension == "":
+                folder.append(items)
+            elif extension != "":
+                files.append(items)
+
     else:
         print("The given path file doesn't exists")
 
 intialize_folder(target_dir)
-
